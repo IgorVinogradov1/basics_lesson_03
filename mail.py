58c03273bf800ba574a9293f6741abeddb624512
IgorVinogradov1 <igor.vinogradov1@yandex.com>
2025-10-31 03:13:02 +0300

    меньше скобок

--- mail.py
+++ mail.py
@@ -6,18 +6,18 @@ import smtplib
 
 login_mail = os.getenv('LOGIN')
 pass_mail = os.getenv('PASSWORD')
-letter = ("""\
-From: {s}
-To: {r}
-Subject: Приглашение!
-Content-Type: text/plain; charset="UTF-8";
+receiver = 'ваш e-mail' # не заполнена почта
+website = 'https://dvmn.org/profession-ref-program/s.vinogradov.28/Stnbw/'
+friend_name = 'Александр'
+my_name = 'Игорь'
+header = f'From: {login_mail}\nTo: {receiver}\nSubject: Приглашение!\nContent-Type: text/plain; charset="UTF-8";\n\n'
+body_letter = f'''\
+Привет, {friend_name}! {my_name} приглашает тебя на сайт {website}!
 
-Привет, {f}! {m} приглашает тебя на сайт {w}!
-
-{w} — это новая версия онлайн-курса по программированию. 
+{website} — это новая версия онлайн-курса по программированию. 
 Изучаем Python и не только. Решаем задачи. Получаем ревью от преподавателя. 
 
-Как будет проходить ваше обучение на {w}? 
+Как будет проходить ваше обучение на {website}? 
 
 → Попрактикуешься на реальных кейсах. 
 Задачи от тимлидов со стажем от 10 лет в программировании.
@@ -26,14 +26,9 @@ Content-Type: text/plain; charset="UTF-8";
 → Подготовишь крепкое резюме.
 Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят. 
 
-Регистрируйся → {w}  
-На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.\
-""")
-receiver = 'ваш e-mail' # не заполнена почта
-website = 'https://dvmn.org/profession-ref-program/s.vinogradov.28/Stnbw/'
-friend_name = 'Александр'
-my_name = 'Игорь'
-letter = letter.format(s=login_mail, r=receiver, w=website, f=friend_name, m=my_name)
+Регистрируйся → {website}  
+На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.'''
+letter = header + body_letter
 letter = letter.encode('UTF-8')
 server = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
 server.login(login_mail, pass_mail)
